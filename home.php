ad7e1b3a64ac5f8365b3a60f6f698567782fd708
Charlie Eldergill <WS343350@weston.ac.uk>
2024-10-20 00:44:28 +0100

    footer

--- home.php
+++ home.php
@@ -103,7 +103,7 @@ if ($selected_navy === "NBN") {
     </main>
 
     <div class="container">
-        <footer class="py-3 my-4">
+        <footer class="navbar-fixed-bottom py-3 my-4">
             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
                 <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Tradelands</a></li>
                 <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Wiki</a></li>
