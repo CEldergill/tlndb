80d172039aeb225e4635f29185c33e0fd7a9943a
Charlie Eldergill <WS343350@weston.ac.uk>
2024-10-20 00:47:03 +0100

    home styling

--- home.php
+++ home.php
@@ -49,6 +49,15 @@ if ($selected_navy === "NBN") {
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <style>
+        html,
+        body {
+            height: 100%;
+            margin: 0;
+            overflow: hidden;
+        }
+    </style>
 </head>
 
 <body>
