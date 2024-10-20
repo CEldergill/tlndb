98b6657871dcdff2c1857771a55d1f9f290b9df0
Charlie Eldergill <WS343350@weston.ac.uk>
2024-10-20 18:41:53 +0100

    debug

--- updateNavyMembers.php
+++ updateNavyMembers.php
@@ -59,6 +59,8 @@ if ($navy_to_process && isset($navies[$navy_to_process])) {
     $usersToAdd = !empty($navyGroupMembers)
         ? array_diff($navyGroupMembers, $navyDbMembers) : [];
 
+    var_dump($usersToAdd);
+
     $usersToAmmend = !empty($navyGroupMembers)
         ? array_diff($navyGroupMembers, $citizensFromDb) : [];
 
