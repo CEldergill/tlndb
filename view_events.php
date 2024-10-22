f7565c31f0ebf73115eec31156b5800bcb60bced
Charlie Eldergill <WS343350@weston.ac.uk>
2024-10-22 14:14:09 +0100

    datables

--- view_events.php
+++ view_events.php
@@ -125,7 +125,7 @@ $events_result = $stmt->get_result();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.jqueryui.min.css">
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
 
 
