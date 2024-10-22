b1f3c537575d53dc9a5a0a35453b302165dc8275
Charlie Eldergill <WS343350@weston.ac.uk>
2024-10-22 14:12:55 +0100

    datatables updated cdn

--- view_events.php
+++ view_events.php
@@ -125,8 +125,8 @@ $events_result = $stmt->get_result();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.min.css">
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
 
 
     <link rel="stylesheet" href="view_events.css">
